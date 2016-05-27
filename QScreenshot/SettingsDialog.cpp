31623d00bf8d0bbe21b9b02cf5623a3db2001a26
winuser <dawid.bautsch@gmail.com>
2016-05-27 12:50:02 +0200

    read/write settings: done

--- QScreenshot/SettingsDialog.cpp
+++ QScreenshot/SettingsDialog.cpp
@@ -1,11 +1,18 @@
 #include "SettingsDialog.h"
 #include "ui_SettingsDialog.h"
 
+#include <QSettings>
+
+static const QString SETTINGS_ORGANIZATION  = QString("Dawid Bautsch");
+static const QString SETTINGS_APPLICATION   = QString("QScreenshot");
+
 SettingsDialog::SettingsDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::SettingsDialog)
 {
     ui->setupUi(this);
+
+    ReadSettings();
 }
 
 SettingsDialog::~SettingsDialog()
@@ -13,11 +20,16 @@ SettingsDialog::~SettingsDialog()
     delete ui;
 }
 
+void SettingsDialog::closeEvent(QCloseEvent *)
+{
+    ReadSettings();
+}
+
 void SettingsDialog::on_pushButton_clicked()
 {
     //!<    save button clicked
 
-
+    WriteSettings();
 }
 
 void SettingsDialog::on_horizontalSlider_sliderMoved(int position)
@@ -32,4 +44,30 @@ void SettingsDialog::on_horizontalSlider_2_sliderMoved(int position)
     //!<    PNG compression quality slider has changed
 
     ui->label_4->setText(QString::number(position));
+}
+
+void SettingsDialog::ReadSettings()
+{
+    QSettings settings(SETTINGS_ORGANIZATION, SETTINGS_APPLICATION);
+
+    ui->horizontalSlider->setValue(settings.value("JPEG_Quality", 80).toInt());
+    ui->horizontalSlider_2->setValue(settings.value("PNG_Quality", 7).toInt());
+}
+
+void SettingsDialog::WriteSettings()
+{
+    QSettings settings(SETTINGS_ORGANIZATION, SETTINGS_APPLICATION);
+
+    settings.setValue("JPEG_Quality", ui->horizontalSlider->value());
+    settings.setValue("PNG_Quality", ui->horizontalSlider_2->value());
+}
+
+void SettingsDialog::on_horizontalSlider_valueChanged(int value)
+{
+    on_horizontalSlider_sliderMoved(value);
+}
+
+void SettingsDialog::on_horizontalSlider_2_valueChanged(int value)
+{
+    ui->label_4->setText(QString::number(value));
 }
--- QScreenshot/SettingsDialog.h
+++ QScreenshot/SettingsDialog.h
@@ -12,18 +12,30 @@ class SettingsDialog : public QDialog
     Q_OBJECT
 
 public:
-    explicit SettingsDialog(QWidget *parent = 0);
-    ~SettingsDialog();
+    explicit                SettingsDialog(QWidget *parent = 0);
+
+                            ~SettingsDialog();
+
+    void                    closeEvent(QCloseEvent *);
 
 private slots:
-    void on_pushButton_clicked();
+    void                    on_pushButton_clicked();
 
-    void on_horizontalSlider_sliderMoved(int position);
+    void                    on_horizontalSlider_sliderMoved(int position);
 
-    void on_horizontalSlider_2_sliderMoved(int position);
+    void                    on_horizontalSlider_2_sliderMoved(int position);
+
+    void on_horizontalSlider_valueChanged(int value);
+
+    void on_horizontalSlider_2_valueChanged(int value);
 
 private:
-    Ui::SettingsDialog *ui;
+    Ui::SettingsDialog *    ui;
+
+
+    void                    ReadSettings();
+
+    void                    WriteSettings();
 };
 
 #endif // SETTINGSDIALOG_H
